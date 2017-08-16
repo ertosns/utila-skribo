c8e2851e384e2e93ff49c134a99a3a202d0fcdfe
root <root@ertosnsbot@yahoo.com>
2017-08-16 22:08:22 +0200

    organize code

--- cq/cq.sh
+++ cq/cq.sh
@@ -12,27 +12,22 @@ if [ $1 == 'c' ]; then
     find . -iname "*.hxx" >> ./cscope.files
     find . -iname "*.hh " >> ./cscope.files
     cscope -cb
-    ctags --fields=+i -n -R -L ./cscope.files
-    cqmakedb -s ./myproject.db -c ./cscope.out -t ./tags -p
+
 #java
 elif [ $1 == 'j' ]; then
     find . -iname "*.java" > ./cscope.files
     cscope -cbR
-    ctags --fields=+i -n -R -L ./cscope.files
-    cqmakedb -s ./myproject.db -c ./cscope.out -t ./tags -p
 #python
 elif [ $1 == 'p' ]; then
     find . -iname "*.py"    > ./cscope.files
     pycscope -i ./cscope.files
-    ctags --fields=+i -n -R -L ./cscope.files
-    cqmakedb -s ./myproject.db -c ./cscope.out -t ./tags -p
 # ruby, go, javascript
 elif [ $1 == 'r' | $1 == 'g' | $1 == 'js' ]; then
     find . -iname "*.rb"    > ./cscope.files
     find . -iname "*.go"    > ./cscope.files
     find . -iname "*.js"    > ./cscope.files
     starscope -e cscope
-    ctags --fields=+i -n -R -L ./cscope.files
-    cqmakedb -s ./myproject.db -c ./cscope.out -t ./tags -p
 fi
+ctags --fields=+i -n -R -L ./cscope.files
+cqmakedb -s ./myproject.db -c ./cscope.out -t ./tags -p
 codequery
