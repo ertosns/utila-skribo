6e3088547d59898c53fae2333a89b5b15c7d6e6f
ertosns <ertosnsbot@yahoo.com>
2017-10-31 13:57:32 +0200

    laboron de tago

--- autotag.sh
+++ autotag.sh
@@ -3,7 +3,7 @@
 #editor have to perform following script upon file change.
 
 cd $1
-rm -p TAGS
+rm -rf TAGS
 for file in $(find $1 -type f -regex ".+\\.c?h?S?\(cpp\)?\(cxx\)?\(cc\)?\(hpp\)?\(hxx\)?\(hh\)?\(rb\)?\(go\)?\(js\)?\(py\)?\(java\)?" ); do
     echo "adding $file"
     etags -a $file
